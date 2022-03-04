f3957430540e9543118240a5794f3782b902d665
Eduardo San Martin Morote <posva13@gmail.com>
2022-03-04 17:03:13 +0100

    build: configs in vue-termui

---
+++ packages/termui/vite.config.ts
@@ -0,0 +1,35 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueTermui',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: [
+        'events',
+        'assert',
+        'node:events',
+        'node:assert',
+        '@vue/runtime-core',
+        'ansi-escapes',
+        'chalk',
+        'cli-boxes',
+        'cli-cursor',
+        'cli-truncate',
+        'indent-string',
+        'slice-ansi',
+        'string-width',
+        'type-fest',
+        'widest-line',
+        'wrap-ansi',
+        'yoga-layout',
+        'yoga-layout-prebuilt',
+      ],
+    },
+  },
+})
