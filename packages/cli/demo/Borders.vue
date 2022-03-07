b8c19b98ba152e5c321e4ab4aab57f040c22217f
Eduardo San Martin Morote <posva13@gmail.com>
2022-03-07 12:37:17 +0100

    more tries

---
+++ packages/cli/demo/Borders.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { TuiBox as Box } from 'vue-termui'
+</script>
+
+<template>
+  <Box flexDirection="column" :padding="2">
+    <Box>
+      <Box borderStyle="single" :marginRight="2">
+        <TuiText>single</TuiText>
+      </Box>
+
+      <Box borderStyle="double" :marginRight="2">
+        <TuiText>double</TuiText>
+      </Box>
+
+      <Box borderStyle="round" :marginRight="2">
+        <TuiText>round</TuiText>
+      </Box>
+
+      <Box borderStyle="bold">
+        <TuiText>bold</TuiText>
+      </Box>
+    </Box>
+
+    <Box :marginTop="1">
+      <Box borderStyle="singleDouble" :marginRight="2">
+        <TuiText>singleDouble</TuiText>
+      </Box>
+
+      <Box borderStyle="doubleSingle" :marginRight="2">
+        <TuiText>doubleSingle</TuiText>
+      </Box>
+
+      <Box borderStyle="classic">
+        <TuiText>classic</TuiText>
+      </Box>
+    </Box>
+  </Box>
+</template>
