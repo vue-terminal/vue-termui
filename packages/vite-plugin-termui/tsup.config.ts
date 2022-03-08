62f3b1ff5f617e97967eeaf28e2f8233079d02f6
Eduardo San Martin Morote <posva13@gmail.com>
2022-03-08 16:38:20 +0100

    feat: add vite plugin

---
+++ packages/vite-plugin-termui/tsup.config.ts
@@ -0,0 +1,12 @@
+import { resolve } from 'path'
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  clean: true,
+  // outDir: resolve(__dirname, './dist'),
+  target: 'node14',
+  format: ['esm'],
+  dts: true,
+  // entry: [resolve(__dirname, 'src/index.ts')],
+  entry: ['src/index.ts'],
+})
