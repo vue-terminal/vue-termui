0a39f0b5bb10bf43925b26244dd39923c2aa7896
Eduardo San Martin Morote <posva13@gmail.com>
2022-03-11 12:29:41 +0100

    add more key codes

--- packages/termui/src/composables/keyboard.ts
+++ packages/termui/src/composables/keyboard.ts
@@ -12,22 +12,63 @@ export interface KeyboardHandlerOptions {
 
 type TODO = any
 
-export interface KeyboardHandlerFn {
-  (event: TODO): void
+export interface KeypressEvent {
+  key: KeyboardEventKeyCode
 }
+
+export interface KeypressEventRaw {
+  input: string
+  key: KeyboardEventKeyCode | undefined
+}
+
+export interface KeyboardEventHandlerFn {
+  (event: KeypressEvent): void
+}
+
+export interface KeyboardEventRawHandlerFn {
+  (event: KeypressEventRaw): void
+}
+
+export type KeyboardEventHandler =
+  | KeyboardEventHandlerFn
+  | KeyboardEventRawHandlerFn
 
 // TODO: can probably infer some types
 
+export type RemoveListener = () => void
+
+export function onKeypress(
+  handler: KeyboardEventRawHandlerFn,
+  options?: TODO
+): RemoveListener
 export function onKeypress(
   key: string,
-  handler: KeyboardHandlerFn,
+  handler: KeyboardEventHandlerFn,
   // maybe for modifiers like ctrl, etc
   options?: TODO
-) {
+): RemoveListener
+export function onKeypress(
+  keyOrHandler: string | KeyboardEventHandler,
+  handlerOrOptions?: KeyboardEventHandler | TODO,
+  // maybe for modifiers like ctrl, etc
+  options?: TODO
+): RemoveListener {
   const eventMap = inject(EventMapSymbol)
   if (!eventMap) {
-    // TODO: error
-    throw new Error('you forgot to call attachKeyboardHandler')
+    // TODO: warning with getCurrentInstance()
+    throw new Error('onKeypress must be called inside setup')
+  }
+
+  let key: string
+  let handler: KeyboardEventHandler
+
+  if (typeof keyOrHandler === 'function') {
+    key = '@any'
+    handler = keyOrHandler
+    options = handlerOrOptions
+  } else {
+    key = keyOrHandler
+    handler = handlerOrOptions
   }
 
   if (!eventMap.has(key)) {
@@ -39,14 +80,15 @@ export function onKeypress(
   onMounted(() => {
     listenerList.add(handler)
   })
-
-  onUnmounted(() => {
+  const removeListener = () => {
     listenerList.delete(handler)
-  })
+  }
+  onUnmounted(removeListener)
+  return removeListener
 }
 
 export const EventMapSymbol = Symbol() as InjectionKey<
-  Map<string, Set<KeyboardHandlerFn>>
+  Map<string, Set<KeyboardEventHandler>>
 >
 
 export function attachKeyboardHandler(
@@ -54,7 +96,10 @@ export function attachKeyboardHandler(
   stdin: NodeJS.ReadStream,
   { setRawMode }: KeyboardHandlerOptions
 ) {
-  const eventMap = new Map<string, Set<KeyboardHandlerFn>>()
+  const eventMap = new Map<string, Set<KeyboardEventHandler>>([
+    // create an any handler
+    ['@any', new Set()],
+  ])
 
   app.provide(EventMapSymbol, eventMap)
 
@@ -62,13 +107,20 @@ export function attachKeyboardHandler(
     const input = String(data)
     console.error(input)
 
-    const key: string | undefined = DataToKey[input]
+    const key = DataToKey.get(input)
+
+    // if (!key) {
+    //   console.error(`⚠️  You need to handle key "${input}"`)
+    // }
 
     if (key && eventMap.has(key)) {
       eventMap.get(key)!.forEach((handler) => {
-        handler({ key })
+        ;(handler as KeyboardEventHandlerFn)({ key })
       })
     }
+    eventMap.get('@any')!.forEach((handler) => {
+      ;(handler as KeyboardEventRawHandlerFn)({ input, key })
+    })
   }
 
   // TODO: take again from here: this must be done only once and we must manually listen to ctrl+c
@@ -86,25 +138,44 @@ export function attachKeyboardHandler(
   }
 }
 
-// map to same event.key as in browser: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
+/**
+ * Emulated keycodes based on browser's map to same event.key as in browser:
+ * https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
+ */
+export type KeyboardEventKeyCode =
+  | 'ArrowUp'
+  | 'ArrowDown'
+  | 'ArrowRight'
+  | 'ArrowLeft'
+  | 'PageUp'
+  | 'PageDown'
+  | 'Enter'
+  | 'Escape'
+  | 'Tab'
+  | 'Tab'
+  | 'Backspace'
+  | 'Delete'
+  | 'Delete'
 
-// TODO: does it work with a Map?
+const DataToKey = new Map<string, KeyboardEventKeyCode>([
+  ['\u001B[A', 'ArrowUp'],
+  ['\u001B[B', 'ArrowDown'],
+  ['\u001B[C', 'ArrowRight'],
+  ['\u001B[D', 'ArrowLeft'],
 
-const DataToKey: Record<string, string> = {
-  '\u001B[A': 'ArrowUp',
-  // down: '\u001B[B',
-  // left: '\u001B[D',
-  // right: '\u001B[C',
-  // pageDown: '\u001B[6~',
-  // pageUp: '\u001B[5~',
-  // enter: '\r',
-  // escape: '\u001B',
-  // tab: '\t',
-  // tab: '\u001B[Z',
-  // backspace: '\u0008',
-  // delete: '\u007F',
-  // delete: '\u001B[3~',
-}
+  ['\u001B[5~', 'PageUp'],
+  ['\u001B[6~', 'PageDown'],
+
+  ['\r', 'Enter'],
+  ['\u001B', 'Escape'],
+
+  ['\t', 'Tab'],
+  ['\u001B[Z', 'Tab'],
+
+  ['\u0008', 'Backspace'],
+  ['\u007F', 'Delete'],
+  ['\u001B[3~', 'Delete'],
+])
 
 export function isRawModeSupported(stdin: NodeJS.ReadStream) {
   return stdin.isTTY
