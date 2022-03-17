19e781432d06145580da25f00a69137358e04c78
Eduardo San Martin Morote <posva13@gmail.com>
2022-03-17 16:16:17 +0100

    refactor: logo

--- packages/cli/demo/CenteredDemo.vue
+++ packages/cli/demo/CenteredDemo.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, onMouseEvent, onInput, onKeypress, reactive } from 'vue-termui'
+import Logo from './VueTermUILogo.vue'
 
 const n = ref(0)
 
@@ -74,32 +75,7 @@ onInput((data) => {
         <Link href="https://esm.dev">Site</Link>
       </span>
     </div>
-    <Box
-      flexDirection="column"
-      borderColor="whiteBright"
-      bgColor="#42B883"
-      :width="20"
-      :height="8"
-      :paddingX="1"
-      borderStyle="double"
-      justifyContent="center"
-      alignItems="flex-start"
-    >
-      <Text color="#FF6AF0">&nbsp;_________</Text>
-      <Text color="#FF6AF0"><Text color="#42B883">/</Text>\__\__\__\</Text>
-      <Text color="#42B883">\/__/<Text color="#FF6AF0">\__\</Text>_/</Text>
-      <Text>&nbsp;&nbsp;&nbsp;&nbsp;<Text color="#42B883">\/__/</Text></Text>
-      <Box
-        alignSelf="flex-end"
-        flexDirection="column"
-        justifyContent="flex-end"
-        alignItems="flex-end"
-        width="100%"
-      >
-        <Text>[V]ue</Text>
-        <Text>[T]ermUI</Text>
-      </Box>
-    </Box>
+    <Logo />
     <div
       :width="40"
       :height="10"
---
+++ packages/cli/demo/VueTermUILogo.vue
@@ -0,0 +1,28 @@
+<template>
+  <Box
+    flexDirection="column"
+    borderColor="whiteBright"
+    bgColor="#42B883"
+    :width="20"
+    :height="8"
+    :paddingX="1"
+    borderStyle="double"
+    justifyContent="center"
+    alignItems="flex-start"
+  >
+    <Text color="#FF6AF0">&nbsp;_________</Text>
+    <Text color="#FF6AF0"><Text color="#42B883">/</Text>\__\__\__\</Text>
+    <Text color="#42B883">\/__/<Text color="#FF6AF0">\__\</Text>_/</Text>
+    <Text>&nbsp;&nbsp;&nbsp;&nbsp;<Text color="#42B883">\/__/</Text></Text>
+    <Box
+      alignSelf="flex-end"
+      flexDirection="column"
+      justifyContent="flex-end"
+      alignItems="flex-end"
+      width="100%"
+    >
+      <Text>[V]ue</Text>
+      <Text>[T]ermUI</Text>
+    </Box>
+  </Box>
+</template>
