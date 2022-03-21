e8a59409043058b115d9e39f24240f894e7251b2
Eduardo San Martin Morote <posva13@gmail.com>
2022-03-21 09:56:54 +0100

    feat(components): wip input

---
+++ packages/termui/src/components/Input.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { getCurrentInstance, onMounted } from '@vue/runtime-core'
+
+const instance = getCurrentInstance()
+
+console.log(!!instance)
+debugger
+
+onMounted(() => {
+  console.log(!!instance)
+  debugger
+})
+</script>
+
+<template>
+  <TuiText dimmed>Label: </TuiText>
+  <TuiText>value</TuiText>
+</template>
