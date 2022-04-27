5f41db6c560c750d78d71509dd3d9b06e737b156
Eduardo San Martin Morote <posva13@gmail.com>
2022-04-27 16:00:02 +0200

    docs: text logo

---
+++ packages/docs/.vitepress/theme/components/TextLogo.vue
@@ -0,0 +1,27 @@
+<template>
+  <pre class="text-logo">
+<span class="T">_________</span>
+<span class="V">/</span><span class="T">\__\__\__\</span>
+<span class="V">\/__/<span class="T">\__\</span>_/</span>
+<span class="V">  \/__/</span>
+         [<span class="bold V">V</span>]ue
+      [<span class="bold T">T</span>]ermUI
+  </pre>
+</template>
+
+<style scoped>
+pre.text-logo {
+  line-height: 1.25;
+}
+pre.text-logo .bold {
+  font-weight: bold;
+}
+
+pre.text-logo .V {
+  color: var(--vt-c-brand-secondary);
+}
+
+pre.text-logo .T {
+  color: var(--vt-c-brand);
+}
+</style>
