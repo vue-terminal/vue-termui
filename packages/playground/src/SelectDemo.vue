d111cb6e75ec5ef9a2df2884314bc86ec4a57f0f
webfansplz <308241863@qq.com>
2022-10-31 21:26:31 +0800

    feat: `Select` component

---
+++ packages/playground/src/SelectDemo.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue-termui'
+const value = ref('vue')
+const options = [
+  {
+    value: 'vue',
+    label: 'Vue',
+  },
+  {
+    value: 'react',
+    label: 'React',
+  },
+  {
+    value: 'svelte',
+    label: 'Svelte',
+  },
+  {
+    value: 'solid',
+    label: 'Solid',
+  },
+]
+
+function submit(name: string, index: number) {
+  console.log(name, index)
+}
+</script>
+
+<template borderStyle="round">
+  <Select v-model="value" @submit="submit">
+    <Option :value="item.value" v-for="(item, index) in options" :key="index">
+      <template v-slot="{ isActive }">
+        <Box flexDirection="column">
+          <Text :dimmed="isActive" :underline="isActive">{{ item.label }}</Text>
+        </Box>
+      </template>
+    </Option>
+  </Select>
+</template>
