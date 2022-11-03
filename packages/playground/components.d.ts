7fa380123b0dd4f676406ce59e7ea016aa0e8b65
webfansplz <308241863@qq.com>
2022-11-03 21:08:49 +0800

    chore: upgrade deps

--- packages/playground/components.d.ts
+++ packages/playground/components.d.ts
@@ -7,13 +7,8 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Box: typeof import('vue-termui')['TuiBox']
-    Br: typeof import('vue-termui')['TuiNewline']
-    Div: typeof import('vue-termui')['TuiBox']
-    Link: typeof import('vue-termui')['TuiLink']
     Newline: typeof import('vue-termui')['TuiNewline']
     Progressbar: typeof import('vue-termui')['TuiProgressBar']
-    Span: typeof import('vue-termui')['TuiText']
     Text: typeof import('vue-termui')['TuiText']
   }
 }
