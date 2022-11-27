ce24f4e8340a8dee9c40ae28d993cf8a575889d1
webfansplz <308241863@qq.com>
2022-11-27 22:44:24 +0800

    build: external node built-in module

--- packages/vite-plugin-vue-termui/src/index.ts
+++ packages/vite-plugin-vue-termui/src/index.ts
@@ -116,6 +116,7 @@ export default function VueTermui({
                 'wrap-ansi',
                 'yoga-layout',
                 'yoga-layout-prebuilt',
+                /^node:/,
               ],
 
               output: {
